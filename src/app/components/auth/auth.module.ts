675a651eaa10a751a5715ce92af210d9356939c7
muhammad awais <awaisshaikh94@gmail.com>
2020-11-05 14:48:07 +0500

    modal components and boostarp fontawesome added done

---
+++ src/app/components/auth/auth.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ForgetComponent } from './forget/forget.component';
+
+
+@NgModule({
+  declarations: [AuthComponent, LoginComponent, SignupComponent, ForgetComponent],
+  imports: [
+    CommonModule,
+    AuthRoutingModule
+  ]
+})
+export class AuthModule { }
---
+++ src/app/components/payment/payment.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PaymentRoutingModule } from './payment-routing.module';
+import { PaymentComponent } from './payment/payment.component';
+
+
+@NgModule({
+  declarations: [PaymentComponent],
+  imports: [
+    CommonModule,
+    PaymentRoutingModule
+  ]
+})
+export class PaymentModule { }

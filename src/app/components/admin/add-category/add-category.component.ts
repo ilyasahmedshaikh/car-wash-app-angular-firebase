65823cbf5d5674dea9534c85cd831fbbc3a0d8ff
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-11-26 01:52:40 +0500

    add category, listing all categories

--- src/app/components/admin/add-category/add-category.component.ts
+++ src/app/components/admin/add-category/add-category.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 
 @Component({
   selector: 'app-add-category',
@@ -7,9 +13,74 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddCategoryComponent implements OnInit {
 
-  constructor() { }
+  programForm: FormGroup;
+
+  categoryCollection: string = "categories";
+  data: any = [];
+
+  preview: any = "../../../../assets/img/img-upload-icon.png";
+  loading: any = "../../../../assets/img/loading.gif";
+  percent: number = 0;
+  uploadPercent: Observable<number>;
+  downloadURL: Observable<string>;
+  imageUploaded: boolean = false;
+
+  constructor(
+    private router: Router,
+    private fireStore: AngularFirestore,
+    private storage: AngularFireStorage,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      name: ['', Validators.required]
+    });
+  }
+
+  addCategory() {
+    this.fireStore.collection(this.categoryCollection).add({
+      image: this.preview,
+      name: this.programForm.value.name,
+    })
+    .then(res => {
+      // console.log(res);
+      this.router.navigateByUrl("/admin/all-categories");
+      this.programForm.reset();
+    })
+    .catch(e => {
+      console.log(e);
+    })
+  }
+
+  uploadFile(event) {
+    this.imageUploaded = !this.imageUploaded;
+    
+    const file = event.target.files[0];
+    const filePath = file.name;
+    const fileRef = this.storage.ref(filePath);
+    const task = this.storage.upload(filePath, file);
+
+    // observe percentage changes
+    this.uploadPercent = task.percentageChanges();
+    this.uploadPercent.subscribe(res => this.percent = res );
+    
+    // get notified when the download URL is available
+    task.snapshotChanges().pipe(
+      finalize(() => {
+        this.downloadURL = fileRef.getDownloadURL();
+        this.downloadURL.subscribe(res => {
+          this.preview = res;
+
+          this.imageUploaded = !this.imageUploaded;
+        });
+      })
+    )
+    .subscribe();
   }
 
 }
--- src/app/components/admin/all-categories/all-categories.component.ts
+++ src/app/components/admin/all-categories/all-categories.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { setTimeout } from 'timers';
 
 @Component({
   selector: 'app-all-categories',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AllCategoriesComponent implements OnInit {
 
-  constructor() { }
+  categoryCollection: string = "categories";
+  data: any = [];
+
+  constructor(
+    private fireStore: AngularFirestore
+  ) {}
 
   ngOnInit(): void {
+    this.getCategories();
+  }
+
+  getCategories() {
+    this.fireStore.collection(this.categoryCollection).get().subscribe((res) => {
+      res.docs.forEach((doc) => {
+        
+        let item = {
+          id: doc.id,
+          image: doc.data()['image'],
+          name: doc.data()['name'],
+        }
+
+        this.data.push(item);
+      });
+    });    
   }
 
 }

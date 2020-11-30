26ffe7c597125f3890bb4b810a9cfee02c9c7be6
ilyasahmedshaikh <ilyasahmed081@gmail.com>
2020-11-30 23:10:09 +0500

    add-service-category added reactive-form

--- src/app/components/service-request/new-service-request/new-service-request.component.ts
+++ src/app/components/service-request/new-service-request/new-service-request.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-new-service-request',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewServiceRequestComponent implements OnInit {
 
-  constructor() { }
+  programForm: FormGroup;
+  step: number = 1;
+
+  categoryData: any = [];
+  packagesData: any = [];
+
+  constructor(
+    private router: Router,
+    private fireStore: AngularFirestore,
+    private storage: AngularFireStorage,
+    private fb: FormBuilder,
+  ) { }
 
   ngOnInit(): void {
+    this.formInit();
+  }
+
+  formInit() {
+    this.programForm = this.fb.group({
+      fullName: ['', Validators.required],
+      mobile: ['', Validators.required],
+      location: ['', Validators.required],
+      category: ['', Validators.required],
+      package: ['', Validators.required],
+      datetime: ['', Validators.required],
+      payment: ['', Validators.required],
+    });
+  }
+
+  next() {
+    if (this.step < 2) {
+      this.step += 1;
+    }
+  }
+
+  back() {
+    if (this.step == 2) {
+      this.step -= 1;
+    }
+  }
+
+  serviceRequest() {
+    console.log(this.programForm.value);
   }
 
 }
--- src/app/components/service-request/service-request.module.ts
+++ src/app/components/service-request/service-request.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// import { SharedModule } from '../../shared/shared.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { ServiceRequestRoutingModule } from './service-request-routing.module';
 import { ServiceRequestComponent } from './service-request.component';
 import { NewServiceRequestComponent } from './new-service-request/new-service-request.component';
 import { ServiceRequestsComponent } from './service-requests/service-requests.component';
 
+import { SharedModule } from '../../shared/shared.module';
 
 @NgModule({
   declarations: [ServiceRequestComponent, NewServiceRequestComponent, ServiceRequestsComponent],
   imports: [
     CommonModule,
     ServiceRequestRoutingModule,
-    // SharedModule
+    FormsModule,
+    ReactiveFormsModule,
+    SharedModule
   ]
 })
 export class ServiceRequestModule { }
